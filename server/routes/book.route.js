04d35a6c7b3c2e6876dbc8a70190b9faebd2b43f
Liz Goff <livingearthcreations@gmail.com>
2018-04-24 16:09:45 -0500

    moving server side to router file

---
+++ server/routes/book.route.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Route();
+// const mongoose = require('mongoose');
+
+
+// move route to /routes/book.route.js
+// Get route for /books
+router.get('/books', (req, res) => {
+
+    // ------- database read or find ------ //
+    
+        Book.find({})
+            .then((dataFromTheDatabase) => {
+            // success and good things happen    
+                console.log('data from database', dataFromTheDatabase);
+                res.send(dataFromTheDatabase);
+    
+            })
+            // error and bad things happen. make sure to put this in
+            // or it may be a security risk as the client can see
+            // the error with your code
+    
+            .catch((error) => {
+                console.log('error with Book.find', error);
+                res.sendStatus(500);
+            });
+    
+    });
+    
+    module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 // const bodyParser = require('body-parser');
+const express = require('express');
 const PORT = process.envPORT || 5000;
 const app = express();
 const mongoose = require('mongoose');
@@ -9,8 +9,11 @@ const mongoose = require('mongoose');
 // eg. const books = require('./modules/booksArray');
 const Book = require('./models/book.schema');
 
+const bookRouter = require('./routes/book.route');
+
 
 // -------------- Connecting to Mongo start --------------- //
+// move this connection to modules/database-connection.js
 
 const databaseUrl = 'mongodb://localhost:27017/library';
 mongoose.connect(databaseUrl);
@@ -24,30 +27,6 @@ mongoose.connection.on('error', (error) => {
 });
 
 // ------------- Connecting to Mongo end ----------------- //
-
-
-// Get route for /books
-app.get('/books', (req, res) => {
-
-// ------- database read or find ------ //
-
-    Book.find({})
-        .then((dataFromTheDatabase) => {
-        // success and good things happen    
-            console.log('data from database', dataFromTheDatabase);
-            res.send(dataFromTheDatabase);
-
-        })
-        // error and bad things happen. make sure to put this in
-        // or it may be a security risk as the client can see
-        // the error with your code
-
-        .catch((error) => {
-            console.log('error with Book.find', error);
-            res.sendStatus(500);
-        });
-
-});
 
 app.listen(PORT, () => {
     console.log(`running on port ${PORT}`);
